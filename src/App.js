c8c81229141d9821d6dc6ad2556b07a5a08be9c3
ChristopherASC22 <109623979+ChristopherASC22@users.noreply.github.com>
2023-02-25 20:20:29 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Student } from "./util";
+
+import {useState} from "react";
+
+let Christopher = new Student("Christopher Robles", 17);
+
+function ButtonContainer(props) {
+
+  const [count, setCount] = useState(0);
+
+  function handleClick(e) {
+    e.preventDefault();
+    setCount(count + 1);
+    alert('You clicked the button! ' +  count + " times");
+  }
+
+  function handleHover(e) {
+    e.preventDefault();
+    alert('You hovered over the button!');
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click me</button>
+      <button onMouseOver={handleHover}>Hover here</button>
+      <h1>{Christopher.name}</h1>
+      <h2>{Christopher.age}</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonContainer name="Christopher Robles" />
     </div>
   );
 }
